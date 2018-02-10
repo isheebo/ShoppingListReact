0eabb3a7d263135d93653196ac57b4cdf0e5db0e
isheebo <kedgar751@gmail.com>
2018-02-10 10:17:49 +0300

    [Feature #154896765] Remove the div wrapping around Table rows
    This allows for a convenient display of the table. Previously, the rows
    were displayed at a slight offset with respect to the Table Headers.
    This commit fixes that issue.

--- src/components/lists/ShoppingListComponent.jsx
+++ src/components/lists/ShoppingListComponent.jsx
@@ -12,55 +12,53 @@ const ShoppingListComponent = ({ shoppinglist, onExecuteAction }) => {
     };
 
     return (
-        <div>
-            <TableRow>
-                <TableRowColumn>
-                    <FlatButton
-                        onClick={() => handleClick({ type: 'view items' })}
-                        style={{ color: '#000', textDecoration: 'none' }}
+        <TableRow>
+            <TableRowColumn>
+                <FlatButton
+                    onClick={() => handleClick({ type: 'view items' })}
+                    style={{ color: '#000', textDecoration: 'none' }}
+                >
+                    {shoppinglist.name}
+                </FlatButton>
+            </TableRowColumn>
+
+            <TableRowColumn style={{ color: 'rgba(127,127,127,1)' }}>
+                {shoppinglist.notify_date}
+            </TableRowColumn>
+
+            <TableRowColumn style={{ color: 'rgba(127,127,127,1)' }}>
+                {Moment(shoppinglist.date_created).format('lll')}
+            </TableRowColumn>
+
+            <TableRowColumn style={{ color: 'rgba(127,127,127,1)' }}>
+                {Moment(shoppinglist.date_modified).format('lll')}
+            </TableRowColumn>
+            <TableRowColumn>
+                <IconButton
+                    iconStyle={{ color: '#00bcd4' }}
+                    onClick={() => handleClick({ type: 'edit shoppinglist' })}
+                >
+                    <FontIcon
+                        className="material-icons"
+                        style={{ marginRight: 24, color: '#00bcd4', padding: 5 }}
                     >
-                        {shoppinglist.name}
-                    </FlatButton>
-                </TableRowColumn>
+                        <i className="material-icons">mode_edit</i>
+                    </FontIcon>
+                </IconButton>
 
-                <TableRowColumn style={{ color: 'rgba(127,127,127,1)' }}>
-                    {shoppinglist.notify_date}
-                </TableRowColumn>
-
-                <TableRowColumn style={{ color: 'rgba(127,127,127,1)' }}>
-                    {Moment(shoppinglist.date_created).format('lll')}
-                </TableRowColumn>
-
-                <TableRowColumn style={{ color: 'rgba(127,127,127,1)' }}>
-                    {Moment(shoppinglist.date_modified).format('lll')}
-                </TableRowColumn>
-                <TableRowColumn>
-                    <IconButton
-                        iconStyle={{ color: '#00bcd4' }}
-                        onClick={() => handleClick({ type: 'edit shoppinglist' })}
+                <IconButton
+                    iconStyle={{ color: 'red' }}
+                    onClick={() => handleClick({ type: 'delete shoppinglist' })}
+                >
+                    <FontIcon
+                        className="material-icons"
+                        style={{ marginRight: 24, padding: 5, color: 'red' }}
                     >
-                        <FontIcon
-                            className="material-icons"
-                            style={{ marginRight: 24, color: '#00bcd4', padding: 5 }}
-                        >
-                            <i className="material-icons">mode_edit</i>
-                        </FontIcon>
-                    </IconButton>
-
-                    <IconButton
-                        iconStyle={{ color: 'red' }}
-                        onClick={() => handleClick({ type: 'delete shoppinglist' })}
-                    >
-                        <FontIcon
-                            className="material-icons"
-                            style={{ marginRight: 24, padding: 5, color: 'red' }}
-                        >
-                            <i className="material-icons">delete</i>
-                        </FontIcon>
-                    </IconButton>
-                </TableRowColumn>
-            </TableRow>
-        </div>
+                        <i className="material-icons">delete</i>
+                    </FontIcon>
+                </IconButton>
+            </TableRowColumn>
+        </TableRow>
     );
 };
 
